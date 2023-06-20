c0c195cba80c7d48bd55f1b4bdabf1d6323ffec1
Prashant Yadav <prashant.yadav@terminus.com>
2023-06-20 13:14:10 +0530

    Added home pageand refactor

--- app.py
+++ app.py
@@ -3,11 +3,18 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route('/')
+def home():
+    """Show Homeage"""
+    return {"status": "OK"}
+
+
 @app.route('/<random_string>')
 def returnBackwardsString(random_string):
     """Reverse and return the provided URI"""
-    return "".join(reversed(random_string))
+    reversed_string = "".join(reversed(random_string))
+    return {"reversed": reversed_string}
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=5000, debug=True)
